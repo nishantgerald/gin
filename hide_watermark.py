89b4f3b0022eb9f5261e488a6d0d5f0ec45f2ae7
Nishant Gerald <nishant.gerald@gmail.com>
2022-09-04 22:54:27 -0400

    created hide_watermark script

---
+++ hide_watermark.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def hide_watermark():
+    hide_streamlit_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                </style>
+                """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    hide_watermark()
